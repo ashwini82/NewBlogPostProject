72b740d025584c4af4cd2366b773b48ba9c8e6fc
ashwini82 <ashwiniatharv1982@gmail.com>
2020-06-24 16:18:34 -0400

    First try for bolg project

---
+++ src/main/java/com/example/demo/BlogPost/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.BlogPost.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost,Long> {
+
+}
